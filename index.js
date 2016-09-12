edae08151e70a350bd1c4038242dfa84580867a9
kain_pl <pushkar.valeriy@gmail.com>
2016-09-12 15:45:47 +0300

    fires for any transports

--- index.js
+++ index.js
@@ -7,25 +7,12 @@ module.exports = function (config, callback) {
         config = {};
     }
 
-    var cluster = require('cluster'),
+    var cookie = require('cookie'),
+        cluster = require('cluster'),
         stickyCluster = require('./lib/sticky-cluster');
 
     var sessionHash = function (req, res) {
-        var ip = (req.connection.remoteAddress || '').split(/\./g);
-        var hash = ip.reduce(function (r, num) {
-            r += parseInt(num, 10);
-            r %= 2147483648;
-            r += (r << 10);
-            r %= 2147483648;
-            r ^= r >> 6;
-            return r;
-        }, config.session_hash_seed);
-        hash += hash << 3;
-        hash %= 2147483648;
-        hash ^= hash >> 11;
-        hash += hash << 15;
-        hash %= 2147483648;
-        return hash >>> 0;
+        return cookie.parse(req.headers.cookie)[config.session_cookie_name];
     };
 
     var logger = {
@@ -39,7 +26,7 @@ module.exports = function (config, callback) {
     config.secure = config.secure || false;
     config.socket = config.socket || true;
     config.session_hash = config.session_hash || sessionHash;
-    config.session_hash_seed = config.session_hash_seed || ~~(0.5 * 1e9);
+    config.session_cookie_name = config.session_cookie_name || 'connect.sid';
     config.delay = config.delay || 1000;
     config.proxy_port = config.proxy_port || process.env.PROXY_PORT || (config.secured ? 443 : 80);
     config.worker_port = config.worker_port || process.env.WORKER_PORT || 8000;
